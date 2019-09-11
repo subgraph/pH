03c4d6aeac08193e8a56c34692b55a3abde86bf8
Bruce Leidl <bruce@subgraph.com>
2019-09-11 16:31:48 -0400

    export all the new classes

--- rust/src/system/mod.rs
+++ rust/src/system/mod.rs
@@ -1,2 +1,16 @@
-#[macro_use]
-pub mod ioctl;
+#[macro_use]pub mod ioctl;
+mod epoll;
+mod errno;
+mod bitvec;
+mod socket;
+mod filedesc;
+mod memfd;
+
+pub use bitvec::BitVec;
+pub use filedesc::{FileDesc, FileFlags};
+pub use memfd::MemoryFd;
+pub use epoll::EPoll;
+pub use errno::{Error,Result,errno_result};
+pub use socket::ScmSocket;
+
+use errno::cvt;
